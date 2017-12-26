5e6ba43de2991754b70b0b7463dcf1f5f7857cc3
Minnie Tumrakdee <Minnie@Minnies-MacBook-Pro.local>
2017-12-26 00:07:21 -0500

    Got connection working

--- connect.py
+++ connect.py
@@ -13,30 +13,36 @@ class CoinbaseExchangeAuth(AuthBase):
     def __call__(self, request):
         timestamp = str(time.time())
         message = timestamp + request.method + request.path_url + (request.body or '')
+        message = message.encode('ascii')
         hmac_key = base64.b64decode(self.secret_key)
         signature = hmac.new(hmac_key, message, hashlib.sha256)
-        signature_b64 = signature.digest().encode('base64').rstrip('\n')
+        #signature_b64 = signature.digest().encode('base64').rstrip('\n')
+        signature_b64 = base64.b64encode(signature.digest()).decode('utf-8')
 
         request.headers.update({
+            'Content-Type': 'application/json',
             'CB-ACCESS-SIGN': signature_b64,
             'CB-ACCESS-TIMESTAMP': timestamp,
             'CB-ACCESS-KEY': self.api_key,
-            'CB-ACCESS-PASSPHRASE': self.passphrase,
-            'Content-Type': 'application/json'
+            'CB-ACCESS-PASSPHRASE': self.passphrase
         })
         return request
 
 with open("/Users/Minnie/Marc-Code/marc-git/secrets.json") as f:
     secrets = json.loads(f.read())
 
-api_url = 'https://api.gdax.com/'
-auth = CoinbaseExchangeAuth(secrets[GDAX_API_KEY], secrets[GDAX_SECRET], secrets[GDAX_PASSPHRASE])
+    api_url = 'https://api.gdax.com/'
+    auth = CoinbaseExchangeAuth(secrets["GDAX_API_KEY"], secrets["GDAX_SECRET"], secrets["GDAX_PASSPHRASE"])
 
-# Get accounts
-r = requests.get(api_url + 'accounts', auth=auth)
-print r.json()
+    # Get accounts
+    r = requests.get(api_url + 'accounts', auth=auth)
+
+    jdict = r.json()
+
+    for jkey in jdict:
+        print (jkey)
+
 # [{"id": "a1b2c3d4", "balance":...
-
 # Place an order
 # order = {
 #    'size': 1.0,
